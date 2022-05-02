c4fbb4335ebecbb2dd7f6a13c69dcc561dbcaacb
Jen <jenniferlynnebarry@gmail.com>
2022-05-02 16:48:55 -0700

    passing all tests

--- server/app.js
+++ server/app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const fs = require('fs');
+const csvToJson = require('csvtojson');
+
 const app = express();
-
-app.use((req, res, next) => {
-// write your logging code here
-
-});
+app.set('json spaces', 2);
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
+    let agent = req.header('user-agent').replace(/,/g,'');
+    let time = new Date().toISOString();
+    let method = req.method;
+    let resource = req.originalUrl; 
+    let version = `HTTP/${req.httpVersion}`;
+    let status = res.statusCode;
 
+    let data = `${agent},${time},${method},${resource},${version},${status}`
+    console.log(data);
+
+    fs.appendFile("./server/log.csv", `\n${data}`, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+    res.status(200).send("ok");
 });
 
 app.get('/logs', (req, res) => {
 // write your code to return a json object containing the log data here
+csvToJson()
+.fromFile("./server/log.csv")
+.then((jsonObj) => {
+    res.json(jsonObj);
+})
 
 });
-
 module.exports = app;
